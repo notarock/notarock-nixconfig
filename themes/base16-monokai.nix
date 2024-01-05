22c995d05f18400cf554bf6d216a8982133823d4
Roch D'Amour <roch.damour@gmail.com>
2024-01-05 01:21:50 -0500

    Swap to monokai theme

---
+++ themes/base16-monokai.nix
@@ -0,0 +1,19 @@
+{
+  # Monokai
+  color0 = "#272822";
+  color1 = "#E74C3C";
+  color2 = "#a6e22e";
+  color3 = "#e6db74";
+  color4 = "#268bd2";
+  color5 = "#F92660";
+  color6 = "#66D9EF";
+  color7 = "#f8f8f2";
+  color8 = "#75715e";
+  color9 = "#fd971f";
+  color10 = "#383830";
+  color11 = "#49483e";
+  color12 = "#75715e";
+  color13 = "#f8f8f2";
+  color14 = "#cc6633";
+  color15 = "#f5f4f1";
+}
